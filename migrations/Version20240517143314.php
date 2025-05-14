7cbdaa2b458d919ad44c4a481ca32bae3329258e
Simeon Goranov <sgoranov@users.noreply.github.com>
2025-05-14 19:47:54 +0300

    Removed unused tables

--- migrations/Version20240517143314.php
+++ migrations/Version20240517143314.php
@@ -25,19 +25,11 @@ final class Version20240517143314 extends AbstractMigration
         $this->addSql('CREATE TABLE auth_code (id UUID NOT NULL, identifier TEXT NOT NULL, user_identifier VARCHAR(255) NOT NULL, client_identifier VARCHAR(255) NOT NULL, expiry_date_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, redirect_uri VARCHAR(500) DEFAULT NULL, scopes TEXT DEFAULT NULL, is_revoked BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN auth_code.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN auth_code.expiry_date_time IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE client (id UUID NOT NULL, identifier VARCHAR(100) NOT NULL, name VARCHAR(100) NOT NULL, redirect_uri VARCHAR(500) NOT NULL, is_confidential BOOLEAN NOT NULL, grant_types TEXT NOT NULL, scopes TEXT DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN client.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE client_secret (id UUID NOT NULL, client_id UUID DEFAULT NULL, expiry_date_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, secret VARCHAR(100) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_904A35619EB6921 ON client_secret (client_id)');
-        $this->addSql('COMMENT ON COLUMN client_secret.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN client_secret.client_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN client_secret.expiry_date_time IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE refresh_token (id UUID NOT NULL, access_token_id UUID DEFAULT NULL, identifier VARCHAR(255) NOT NULL, expiry_date_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, is_revoked BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_C74F21952CCB2688 ON refresh_token (access_token_id)');
         $this->addSql('COMMENT ON COLUMN refresh_token.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN refresh_token.access_token_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN refresh_token.expiry_date_time IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE client_secret ADD CONSTRAINT FK_904A35619EB6921 FOREIGN KEY (client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE refresh_token ADD CONSTRAINT FK_C74F21952CCB2688 FOREIGN KEY (access_token_id) REFERENCES access_token (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
@@ -45,12 +37,9 @@ final class Version20240517143314 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE client_secret DROP CONSTRAINT FK_904A35619EB6921');
         $this->addSql('ALTER TABLE refresh_token DROP CONSTRAINT FK_C74F21952CCB2688');
         $this->addSql('DROP TABLE access_token');
         $this->addSql('DROP TABLE auth_code');
-        $this->addSql('DROP TABLE client');
-        $this->addSql('DROP TABLE client_secret');
         $this->addSql('DROP TABLE refresh_token');
     }
 }
